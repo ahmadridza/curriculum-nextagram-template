571be0d943e6437f803a346b39811856f49b4fc3
ahmadridza <59468419+ahmadridza@users.noreply.github.com>
2020-02-26 14:51:19 +0800

    addingadd users to database

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+import peeweedbevolve
+from flask import Flask, render_template, request, flash, Blueprint
+from models.user import User
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -9,28 +10,47 @@ users_blueprint = Blueprint('users',
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
+    # display form
 
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    user_name = request.form.get("user_name")
+    user_email = request.form.get("user_email")
+    user_password = request.form.get("user_password")
+
+    user = User(name=user_name, email=user_email, password=user_password)
+
+    try:
+        user.save()
+        # flash(f"Saved store:{store_name}")
+        return redirect(url_for("users.new"))
+
+    except:
+        # flash("That name is already taken")
+        # linking and displaying at the specific file
+        return render_template("users/new.html")
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
     pass
+# display single user
 
 
 @users_blueprint.route('/', methods=["GET"])
 def index():
     return "USERS"
+# display all users
 
 
 @users_blueprint.route('/<id>/edit', methods=['GET'])
 def edit(id):
     pass
+# edit users details
 
 
 @users_blueprint.route('/<id>', methods=['POST'])
 def update(id):
     pass
+# funtion to update details
